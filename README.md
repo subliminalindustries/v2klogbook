2a11e1748d2c16d0178f5093f0b5dea59f4178ad
Daniel R. Azulay <daniel@danielazulay.eu>
2023-08-23 21:33:03 +0200

    Update

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # v2klogbook
+
 A logbook parser for victims of neuroweapons.
+
+## Format
+
+Every day when you start logging, add a single line with the date like:
+
+`date: 23-8-2023`
+
+After this, add a line for each record, in the format:
+
+`HH:MM {microwave auditory effect subjective loudness - see below}, your entry in text form`
+
+Example:
+
+`12:31 medium, I was making toast and felt a sharp pain in my left hand, the voices said my bread is poisoned.`
+
+## Details for creating an entry:
+
+### Keywords
+
+The script tests whether the words "pain", "threat", "taunt", "paraesthesia",
+or "contraction" are present in each line.
+
+### Tinnitus score
+
+For tinnitus, the following statements can be added to a line:
+- "tinnitus start(ed)"
+- "tinnitus continue(s)"
+- "tinnitus increase(d)"
+- "tinnitus decrease(d)"
+- "tinnitus start(ed)"
+
+This script will follow the degree of tinnitus and encode it.
+
+### Microwave auditory effect subjective loudness
+
+For microwave auditory effect (v2k) subjective loudness, the following
+can be added after the time and before the rest of the line terminated by a comma (','):
+- "low"
+- "medium"
+- "high"
+- "extreme"
+
+
--- main.py
+++ main.py
@@ -1,36 +1,17 @@
 import matplotlib.pyplot as plt
-
-
-def set_ax_size(w=None, h=None, ax=None):
-    if not ax:
-        ax = plt.gca()
-
-    if not w:
-        w = ax.figure.get_width()
-
-    if not h:
-        h = ax.figure.get_height()
-
-    ax_l = ax.figure.subplotpars.left
-    ax_r = ax.figure.subplotpars.right
-    ax_t = ax.figure.subplotpars.top
-    ax_b = ax.figure.subplotpars.bottom
-
-    fig_w = float(w)/(ax_r-ax_l)
-    fig_h = float(h)/(ax_t-ax_b)
-
-    ax.figure.set_size_inches(fig_w, fig_h)
+import numpy as np
 
 
 if __name__ == '__main__':
-    logbook = {'datetime': [],
-               'voice_intensity': [],
+    logbook = {'voice_intensity': [],
+               'tinnitus': [],
                'pain': [],
                'paraesthesia': [],
                'contraction': [],
                'taunt': [],
                'threat': [],
-               'tinnitus': []}
+               'text': [],
+               'datetime': []}
 
     with open('data/logbook.txt', 'r') as fp:
         line = fp.readline()
@@ -63,7 +44,6 @@ if __name__ == '__main__':
                                'extreme': 4
                                }.get(voice_intensity)
 
-
             logbook['voice_intensity'].append(voice_intensity)
             logbook['pain'].append(str(line.find('pain') != -1))
             logbook['paraesthesia'].append(repr(line.find('paraesthesia') != -1))
@@ -81,18 +61,57 @@ if __name__ == '__main__':
                 tinnitus -= 1
 
             logbook['tinnitus'].append(tinnitus)
+            logbook['text'].append(line)
 
             line = fp.readline()
 
+    def hover(event):
+        try:
+            xdata = event.xdata
+            ydata = event.ydata
+            x = int(np.round(xdata))
+            y = int(np.round(ydata))
+
+            for i, art in enumerate(artists):
+                an = annotations[i]
+                cont, data = art.contains(event)
+                if cont:
+                    an.xy = (x, y)
+                    an.set_text(logbook['text'][x])
+                    an.set_visible(True)
+                else:
+                    an.set_text("")
+                    an.set_visible(False)
+
+            fig.canvas.draw_idle()
+        except TypeError:
+            pass
+
+    artists = []
+    annotations = []
+    colors = ['red', 'cyan', 'green', 'purple', 'magenta', 'teal', 'orange']
+
+    plt.rcParams['axes.xmargin'] = 0
+    plt.rcParams['axes.ymargin'] = 0
     fig, axs = plt.subplots(7, 1, sharex='all')
+    plt.subplots_adjust(wspace=.1, hspace=.6)
+
     for idx, k in enumerate(logbook.keys()):
-        if k == 'datetime':
-            continue
+        if k == 'datetime' or k == 'text':
+            break
 
-        axs[idx - 1].plot(logbook['datetime'], logbook[k])
-        axs[idx - 1].set_ylabel(k)
-        axs[idx - 1].set_xticks(ticks=logbook['datetime'], labels=logbook['datetime'], rotation=90)
+        ax = axs[idx]
 
-        # set_ax_size(h=20, ax=axs[idx - 1])
+        annot = ax.annotate('', xy=(0, 0), xytext=(5, 5), textcoords='offset points',
+                            bbox=dict(boxstyle='round', fc='w'))
+        annot.set_visible(False)
+        annotations.append(annot)
 
+        artist = ax.plot(logbook['datetime'], logbook[k], 'o-', label=k, color=colors[idx])
+        artists.append(artist[0])
+
+        ax.set_title(k)
+        ax.set_xticks(ticks=logbook['datetime'], labels=logbook['datetime'], rotation=90)
+
+    fig.canvas.mpl_connect('motion_notify_event', hover)
     plt.show()
